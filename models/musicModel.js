2398d9d2e9c06b5c2117b568f15352b475933220
Patrick Furtak <patrick@patrickfurtak.com>
2020-03-04 16:00:57 -0500

    added album population on index, adjusted anchor tags for routes

--- models/musicModel.js
+++ models/musicModel.js
@@ -1,3 +1,34 @@
 const db = require('./conn');
 
+class MusicModel {
+  constructor(id, name, band, release_year, category) {
+    this.id = id;
+    this.name = name;
+    this.address = band;
+    this.release_year = release_year;
+    this.category = category;
+  }
+  static async getAllAlbums() {
+    try {
+      const resonse = await db.any(`SELECT *
+          FROM albums;`);
+      return resonse;
+    } catch (error) {
+      console.error('error: ', error);
+      return error;
+    }
+  }
+  static async getById(id) {
+    try {
+      const res = await db.any(`SELECT *
+      FROM albums
+      WHERE albums.id = ${id};`);
+      console.log(res);
+      return res;
+    } catch (error) {
+      console.error('ERROR: ', error);
+    }
+  }
+}
+
 module.exports = MusicModel;
--- routes/index.js
+++ routes/index.js
@@ -1,10 +1,12 @@
 const express = require('express');
 const router = express.Router();
+const musicModel = require('../models/musicModel');
 
 /* GET home page. */
 router.get('/', async function(req, res, next) {
+  const data = await musicModel.getAllAlbums();
   res.render('template', {
-    locals: { title: 'Music' },
+    locals: { title: 'Albums', data: data },
     partials: {
       partial: 'partial-index'
     }
